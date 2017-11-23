c76b6bb4283b3c12edf9840c065b5292735a4e25
Mateusz Niestrój <niestroj.mateusz@gmail.com>
2017-11-23 21:09:14 +0100

    Add tests for view

--- L3 - Creating Single Page App/music_albums/public/integration/javascripts/spec/albums_spec.js
+++ L3 - Creating Single Page App/music_albums/public/integration/javascripts/spec/albums_spec.js
@@ -1,13 +1,21 @@
 describe("Albums collection", function() {
-  it("fetches a collection of three albums", function(){
+  it("fetches a collection of three albums", function(done){
     var albumsLoaded = App.albumsLoaded;
     App.albumsLoaded = function() {
       albumsLoaded.apply(App, arguments);
       expect(App.albums.models.length).toBe(3);
-      expect(App.albums.models[0].attributes.title).toBe("string");
+      expect(typeof App.albums.models[0].attributes.title).toBe("string");
       done();
     };
 
     App.init()
+  });
+  it("sets a track_url property when models are created", function(done){
+    App.albumsLoaded = function() {
+      expect(App.albums.first().get("tracks_url")).toMatch(/\/album/);
+      done();
+    };
+
+    App.init();
   });
 });
